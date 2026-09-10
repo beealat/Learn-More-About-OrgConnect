1c61d108cd7328ad8632ee9deedf93108d916c9d
beealat <beachristinacale@gmail.com>
2026-09-10 13:28:31 +0800

    Delete src directory

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './prototype.css'
-import './walkthrough.css'
-
-createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><App /></React.StrictMode>
-)
